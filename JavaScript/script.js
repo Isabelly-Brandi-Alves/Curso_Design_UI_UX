1a286d6d49dc60f22645a4ff5824dad74cb7d505
Isabelly-Brandi-Alves <56138013+Isabelly-Brandi-Alves@users.noreply.github.com>
2023-10-06 10:09:50 -0300

    Atualizando o script.jr, definindo as funções.

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,1 +1,23 @@
 console.log("algumacoisa");
+
+let visor = document.getElementById('visor');
+let expressao = '';
+
+function adicionarCaracter(caracter) {
+    expressao += caracter;
+    visor.value = expressao;
+}
+
+function limparVisor() {
+    expressao = '';
+    visor.value = '';
+}
+
+function calcularResultado() {
+    try {
+        expressao = eval(expressao);
+        visor.value = expressao;
+    } catch (error) {
+        visor.value = 'Erro';
+    }
+}
